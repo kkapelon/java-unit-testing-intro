246e4364590e06820450dee070ac4402593b1e22
Kostis Kapelonis <kkapelon@users.noreply.github.com>
2025-08-12 17:47:19 +0000

    First commit

---
+++ src/main/java/com/codepipes/BasketWeightCalculator.java
@@ -0,0 +1,14 @@
+public class BasketWeightCalculator {
+
+    private int totalWeight = 0;
+
+    public void addItem(int itemWeight) // Assume weight is always an integer number
+    {
+        totalWeight = totalWeight + itemWeight;
+    }
+
+    public int getTotalWeight() {
+        return totalWeight;
+    }
+
+}
---
+++ src/test/java/com/codepipes/BasketWeightTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class BasketWeightTest {
+
+    @Test
+    public void oneItem() {
+        BasketWeightCalculator weightCalculator = new BasketWeightCalculator();
+        weightCalculator.addItem(5);
+
+        assertEquals("Expected 5", 5, weightCalculator.getTotalWeight());
+    }
+
+    @Test
+    public void twoItems() {
+        BasketWeightCalculator weightCalculator = new BasketWeightCalculator();
+        weightCalculator.addItem(5);
+        weightCalculator.addItem(13);
+
+        assertEquals("Expected 18", 18, weightCalculator.getTotalWeight());
+    }
+
+    @Test
+    public void orderDoesNotMatter() {
+        BasketWeightCalculator weightCalculator1 = new BasketWeightCalculator();
+        weightCalculator1.addItem(5);
+        weightCalculator1.addItem(13);
+
+        BasketWeightCalculator weightCalculator2 = new BasketWeightCalculator();
+        weightCalculator2.addItem(13);
+        weightCalculator2.addItem(5);
+
+        assertEquals("Expected 18", 18, weightCalculator1.getTotalWeight());
+        assertEquals("Expected 18", 18, weightCalculator2.getTotalWeight());
+    }
+}
